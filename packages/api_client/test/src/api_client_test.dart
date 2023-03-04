b66436ee8888edccb09c91aa3b5a01502bea7a13
Oscar <martinm.oscar@gmail.com>
2023-03-04 19:31:02 +0100

    chore: demo package

---
+++ packages/api_client/test/src/api_client_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:api_client/api_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('ApiClient', () {
+    test('can be instantiated', () {
+      expect(ApiClient(), isNotNull);
+    });
+  });
+}
