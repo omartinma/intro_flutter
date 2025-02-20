d682fb9815c60195afa2b8711ed75a8309573f8d
Oscar <martinm.oscar@gmail.com>
2025-02-20 10:04:55 +0100

    chore: update analysis options and move test (#17)

--- test/characters/view/characters_page_test.dart
+++ test/characters/view/characters_page_test.dart
@@ -1,8 +1,6 @@
-// ignore_for_file: prefer_const_constructors
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:intro_flutter/character_details/character_details.dart';
 import 'package:intro_flutter/characters/characters.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:rick_and_morty_api/rick_and_morty_api.dart';
@@ -15,7 +13,7 @@ class _MockCharactersBloc extends MockBloc<CharactersEvent, CharactersState>
 void main() {
   group('CharactersPage', () {
     testWidgets('displays a CharactersView', (tester) async {
-      await tester.pumpApp(CharactersPage());
+      await tester.pumpApp(const CharactersPage());
       expect(find.byType(CharactersView), findsOneWidget);
     });
   });
@@ -37,11 +35,11 @@ void main() {
 
     testWidgets('displays CharactersLoadingView when CharactersStatus.loading',
         (tester) async {
-      when(() => charactersBloc.state).thenReturn(CharactersState());
+      when(() => charactersBloc.state).thenReturn(const CharactersState());
       await tester.pumpApp(
         BlocProvider.value(
           value: charactersBloc,
-          child: CharactersView(),
+          child: const CharactersView(),
         ),
       );
       expect(find.byType(CharactersLoadingView), findsOneWidget);
@@ -50,12 +48,12 @@ void main() {
     testWidgets('displays CharactersErrorView when CharactersStatus.failure',
         (tester) async {
       when(() => charactersBloc.state).thenReturn(
-        CharactersState(status: CharactersStatus.failure),
+        const CharactersState(status: CharactersStatus.failure),
       );
       await tester.pumpApp(
         BlocProvider.value(
           value: charactersBloc,
-          child: CharactersView(),
+          child: const CharactersView(),
         ),
       );
       expect(find.byType(CharactersErrorView), findsOneWidget);
@@ -65,7 +63,7 @@ void main() {
         'displays CharactersLoadedView when '
         'CharactersStatus.success with characters', (tester) async {
       when(() => charactersBloc.state).thenReturn(
-        CharactersState(
+        const CharactersState(
           status: CharactersStatus.success,
           characters: characters,
         ),
@@ -73,31 +71,10 @@ void main() {
       await tester.pumpApp(
         BlocProvider.value(
           value: charactersBloc,
-          child: CharactersView(),
+          child: const CharactersView(),
         ),
       );
       expect(find.byType(CharactersLoadedView), findsOneWidget);
-    });
-
-    testWidgets('navigates to character details when clicking on character',
-        (tester) async {
-      when(() => charactersBloc.state).thenReturn(
-        CharactersState(
-          status: CharactersStatus.success,
-          characters: characters,
-        ),
-      );
-      await tester.pumpApp(
-        BlocProvider.value(
-          value: charactersBloc,
-          child: CharactersView(),
-        ),
-      );
-      expect(find.byType(CharactersLoadedView), findsOneWidget);
-      await tester.tap(find.byType(CharacterItemView));
-      await tester.pumpAndSettle();
-      expect(find.byType(CharactersLoadedView), findsNothing);
-      expect(find.byType(CharacterDetailsPage), findsOneWidget);
     });
   });
 }
